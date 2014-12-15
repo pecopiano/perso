2e70d9e0dd63df22edf7f7334a5ff9a0f3008107
pecopiano <firezemissile@gmail.com>
2014-12-15 12:00:48 +0100

    Update top-youtube-links-on-reddit.py

--- top-youtube-links-on-reddit.py
+++ top-youtube-links-on-reddit.py
@@ -3,31 +3,36 @@ from bs4 import BeautifulSoup
 import re
 import csv
 
+# Fetch the page indexing the top voted Youtube links on reddit
 url = 'http://www.reddit.com/domain/youtube.com/top/?sort=top&t=all'
 
 url_read = urllib2.urlopen(url)
 content = url_read.read()
 
+# Make it pretty.
+# For some reason the subsequent regex script didn't catch the Youtube urls on the raw HTML source
 soup = BeautifulSoup(content)
 
 soup_pretty = soup.prettify()
 
+# Encode in utf. Again, the script failed otherwise.
 soup_utf = soup_pretty.encode('utf-8')
 
-match = re.findall('v=(.{11}).*', soup_utf)
+# Regex to find the video id's
+youtube_id = re.findall('v=(.{11}).*', soup_utf)
 
-clean = [p for p in set(match)]
+# Remove duplicates
+clean = [p for p in set(youtube_id)]
 
 url = []
 
+# Append id's to regular Youtube urls
 for p in clean:
     url.append("https://www.youtube.com/watch?v=%s" %p)
 
-for p in url:
-	print p
-
+# Write the resulting urls to a .txt file
 urlss = "\n".join(url)
 
-filename = open("reddit.txt", 'w')
+filename = open("reddit-youtube-links.txt", 'w')
 filename.write(urlss)
 filename.close()
